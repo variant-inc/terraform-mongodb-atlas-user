693c0cc00a2e64f812c61d701e8cb40ac2251de8
RohitSaini04 <rsaini@usxpress.com>
2024-07-02 11:04:40 +0530

    fix pre-commit

--- variables.tf
+++ variables.tf
@@ -52,11 +52,6 @@ variable "namespace" {
   description = "(Required) Namespace where certificate resource will be created"
 }
 
-# variable "certificate_name" {
-#   type        = string
-#   description = "(Required) Name of the certificate created"
-# }
-
 variable "certificate_labels" {
   type        = map(string)
   description = "(Optional) Labels on certificate resource"
