f1349736e890113bebafa8cc839ed8fad7737fbe
RohitSaini04 <rsaini@usxpress.com>
2024-07-02 11:02:11 +0530

    update versions.tf

--- versions.tf
+++ versions.tf
@@ -2,6 +2,10 @@ terraform {
   required_version = "~> 1.1"
   backend "s3" {}
   required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
     mongodbatlas = {
       source  = "mongodb/mongodbatlas"
       version = "~> 1.0"
