68f6473197fea20730eb18f88dfc4121edd650a6
parulpachoriusx <162458540+parulpachoriusx@users.noreply.github.com>
2024-07-10 01:30:34 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,9 @@ output "id" {
   value       = mongodbatlas_database_user.this.id
   description = "Atlas User id"
 }
+
+output "project_id" {
+  value       = data.mongodbatlas_project.project.id
+  description = "Mongodb Atlas id"
+}
+
